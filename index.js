d1d4c51331c7ac2dcce8990e5ab7af47d0c72c67
akash <akash@gmail.com>
2023-03-29 15:57:44 +0530

    cors update

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ app.use(cors({
         const allowed = [
             "http://localhost:5173",
             "http://localhost:3000",
-            // "http://localhost:5000",
-            "https://www.google.com",
-            // "https://www.google.com/"
+            "https://railway-mern-production-e766.up.railway.app"
         ]
         if (allowed.indexOf(o) !== -1 || !o) {
             cb(null, true)
